86c7af0962c1bf8ec7de2aee4bf1cf78e08a0034
Raunak <dhanrajpardesi2@example.com>
2026-01-02 21:50:46 +0530

    Add Edusity project files

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <div className="container">
+        <Title subtitle='Our PROGRAMS' title='What We Offer'/>
+         <Programs/>
+         <About/>
+          <Title subtitle='Gallery' title='Campus Photos'/>
+          <Campus/>
+                    <Title subtitle='TESTIMONIALS' title='What Student Says'/>
+
+
+
+      </div>
+     
+    </div>
+  )
+}
+
+export default App
